7a1a652dc526a0229bb66de5613ff4430a01485f
kroy@sfu.ca <kroy@sfu.ca>
2016-10-14 20:13:01 +0000

    First commit, start accepts args, opens db

---
+++ start.jl
@@ -0,0 +1,15 @@
+#=start.jl - Starts the game and accepts 3 command line arguments,<filename> => database
+<type> => gameType, <cheating> => cheating
+=#
+module start
+
+using SQLite
+
+database = ARGS[1] #/path/to/database/file {string}
+gameType = ARGS[2] #Either "S" or "M" {string}
+cheatingDict = Dict("T" => true, "F" => false)
+cheating = cheatingDict[ARGS[3]] #Either true or false {bool}
+
+SQLite.connect(database) #Opens the database gamefile
+
+end
