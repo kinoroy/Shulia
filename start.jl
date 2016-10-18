e3519be5916e7ce8bb968194affef0c64ad526d9
kroy@sfu.ca <kroy@sfu.ca>
2016-10-18 19:43:47 +0000

    start.jl initializes a board array(of type square) to store gameboard data

--- start.jl
+++ start.jl
@@ -12,4 +12,26 @@ cheating = cheatingDict[ARGS[3]] #Either true or false {bool}
 
 db = SQLite.DB(database) #Opens the database gamefile
 
+#=----Initialize the board ----=#
+board = fill!(Array(square,9,9),square())
+board[1,5] = square('k','w')
+board[9,5] = square('k','b')
+board[1,[4,6]] = square('g','w')
+board[9,[4,6]] = square('g','b')
+board[1,[3,7]] = square('s','w')
+board[9,[3,7]] = square('s','b')
+board[1,[2,8]] = square('n','w')
+board[9,[2,8]] = square('n','b')
+board[1,[1,9]] = square('l','w')
+board[9,[1,9]] = square('l','b')
+board[2,2] = square('b','w')
+board[8,8] = square('b','b')
+board[2,8] = square('r','w')
+board[8,2] = square('r','b')
+board[3,1:9] = square('p','w')
+board[7,1:9] = square('p','b')
+
+#=----Save the board to file----=#
+writedlm("board.txt",board)
+
 end
