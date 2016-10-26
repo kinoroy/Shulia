1b035c0f47ad349060e8ecb89f7e2db4af4380f1
rezyder <rezyder>
2016-10-26 03:03:35 +0000

    move.jl working but straight up resigns

--- move.jl
+++ move.jl
@@ -16,7 +16,7 @@ module move
 
   #= ---- Determines whos turn it is (white or black) ---- =#
 
-  res = SQLite.query(db,"SELECT MAX(move_number) FROM moves;") #Finds the last played move (maximum move_number)
+  res = SQLite.query(db, """SELECT max("move_number") from moves;""") #Finds the last played move (maximum move_number)
   lastMoveIDNullable = res[1,1] #SQL query with max move_number (POSSIBLY "NULL" if no moves have been made)
 
   if (!isnull(lastMoveIDNullable)) #Checks that lastMoveID was not NULL
@@ -29,53 +29,6 @@ module move
 
   #=---- Determines a move to make (AI integration below)----=#
   move_number = lastMoveID+1 #The current move is move#: move_number
-
-
-
-
-
-
-
-
-
-  #Todo: write AI: must define variables: move_type, sourcex, sourcey, targetx, targety, option, i_am_cheating
-
-  # get all of our pieces on board set numbers to them  ####if piece is our piece its choosable
-
-  create array
-
-  # check every square if empty square dont add to list, if piece colour = opponent dont add, if ours add
-
-  if piece colour = our colour add to list
-  create temppiece
-
-  # choose random piece from list based on random number from list (rand mod listend)
-
-  find array length
-  do temppiece = rand % arraylength
-
-  # set temp piece to it
-  # use validate file to see what moves it can make, number them, put them in a list  ####movevalidate
-
-  create another array, if piece -> target = movevalidate, add to list,
-
-  # make a move based on random number from list (rand mod listend)
-
-  makemove = rand % array2length
-
- #random number part
- #use random function mod max val it can be
-
-
-
-
-
-
-
-
-  # ---- Saves the determined move in the database ---- =#
-  SQLite.query(db,"INSERT INTO moves (move_number, move_type, sourcex, sourcey, targetx, targety, option, i_am_cheating)
-  VALUES ($(move_number),$(move_type),$sourcex, $sourcey,$targetx, $targety, $option, $(i_am_cheating));")
 
 
 
@@ -110,6 +63,64 @@ module move
     end
     return false
   end
+
+
+  end
+
+move_type = "resign"
+sourcex = 3
+sourcey = 4
+targetx = 3
+targety = 3
+option = 0
+i_am_cheating = 0
+
+
+
+#=
+  #Todo: write AI: must define variables: move_type, sourcex, sourcey, targetx, targety, option, i_am_cheating
+
+  # get all of our pieces on board set numbers to them  ####if piece is our piece its choosable
+
+  create array
+
+  # check every square if empty square dont add to list, if piece colour = opponent dont add, if ours add
+
+  if piece colour = our colour add to list
+  create temppiece
+
+  # choose random piece from list based on random number from list (rand mod listend)
+
+  find array length
+  do temppiece = rand % arraylength
+
+  # set temp piece to it
+  # use validate file to see what moves it can make, number them, put them in a list  ####movevalidate
+
+  create another array, if piece -> target = movevalidate, add to list,
+
+  # make a move based on random number from list (rand mod listend)
+
+  makemove = rand % array2length
+
+ #random number part
+ #use random function mod max val it can be
+=#
+
+
+#=
+if isCheck == true
+  SQLite.query(db,"INSERT INTO moves (move_number, move_type, sourcex, sourcey, targetx, targety, option, i_am_cheating)
+  VALUES ($(move_number),$(move_type),$sourcex, $sourcey,$targetx, $targety, $option, $(i_am_cheating));")
+=#
+
+
+
+  # ---- Saves the determined move in the database ---- =#
+  SQLite.query(db,"""INSERT INTO moves (move_number, move_type, sourcex, sourcey, targetx, targety, option, i_am_cheating)
+  VALUES ("$(move_number)","$(move_type)","$sourcex","$sourcey","$targetx","$targety", "$option", "$(i_am_cheating)")""";)
+
+
 
 
 end
