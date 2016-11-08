d8ebf74201267cfc1865fc62858ebfa12ce038e2
rezyder <rezyder>
2016-11-08 06:31:56 +0000

    displaygfx final

--- display_gfx.jl
+++ display_gfx.jl
@@ -1,748 +1,120 @@
+#include("dParse.jl")
+
+using Tk
 
 
+
+  iboard = ST.loadBoard()
+  board = Array(Tuple{Char,Char},9,9)
+  for x in 1:size(board)[1]
+    for y in 1:size(board)[2]
+      board[y,x]=(iboard[x,y].piece,iboard[x,y].team)
+    end
+  end
 
 #FOR SHOGI
 
 
-w = Toplevel("Grid")
-f = Frame(w, padding = 10); pack(f, expand=true, fill="both")
 
 
-#ROW1
-b11 = Button(f, "")
-b12 = Button(f, "")
-b13 = Button(f, "")
-b14 = Button(f, "")
-b15 = Button(f, "")
-b16 = Button(f, "")
-b17 = Button(f, "")
-b18 = Button(f, "")
-b19 = Button(f, "")
+bishoppic = ("bishop.gif")
+bishopimg = Image(bishoppic)
+gold_generalpic = ("gold_general.gif")
+gold_generalimg = Image(gold_generalpic)
+kingpic = ("king.gif")
+kingimg = Image(kingpic)
+knightpic = ("knight.gif")
+knightimg = Image(knightpic)
+lancepic = ("lance.gif")
+lanceimg = Image(lancepic)
+pawnpic = ("pawn.gif")
+pawnimg = Image(pawnpic)
+promoted_bishoppic = ("promoted_bishop.gif")
+promoted_bishopimg = Image(promoted_bishoppic)
+promoted_knightpic = ("promoted_knight.gif")
+promoted_knightimg = Image(promoted_knightpic)
+promoted_lancepic = ("promoted_lance.gif")
+promoted_lanceimg = Image(promoted_lancepic)
+promoted_pawnpic = ("promoted_pawn.gif")
+promoted_pawnimg = Image(promoted_pawnpic)
+promoted_rookpic = ("promoted_rook.gif")
+promoted_rookimg = Image(promoted_rookpic)
+promoted_silver_generalpic = ("promoted_silver_general.gif")
+promoted_silver_generalimg = Image(promoted_silver_generalpic)
+rookpic = ("rook.gif")
+rookimg = Image(rookpic)
+silver_generalpic = ("silver_general.gif")
+silver_generalimg = Image(silver_generalpic)
+emptypic = ("empty.gif")
+emptyimg = Image(emptypic)
+
+bishoprpic = ("bishopr.gif")
+bishoprimg = Image(bishoprpic)
+gold_generalrpic = ("gold_generalr.gif")
+gold_generalrimg = Image(gold_generalrpic)
+kingrpic = ("kingr.gif")
+kingrimg = Image(kingrpic)
+knightrpic = ("knightr.gif")
+knightrimg = Image(knightrpic)
+lancerpic = ("lancer.gif")
+lancerimg = Image(lancerpic)
+pawnrpic = ("pawnr.gif")
+pawnrimg = Image(pawnrpic)
+promoted_bishoprpic = ("promoted_bishopr.gif")
+promoted_bishoprimg = Image(promoted_bishoprpic)
+promoted_knightrpic = ("promoted_knightr.gif")
+promoted_knightrimg = Image(promoted_knightrpic)
+promoted_lancerpic = ("promoted_lancer.gif")
+promoted_lancerimg = Image(promoted_lancerpic)
+promoted_pawnrpic = ("promoted_pawnr.gif")
+promoted_pawnrimg = Image(promoted_pawnrpic)
+promoted_rookrpic = ("promoted_rookr.gif")
+promoted_rookrimg = Image(promoted_rookrpic)
+promoted_silver_generalrpic = ("promoted_silver_generalr.gif")
+promoted_silver_generalrimg = Image(promoted_silver_generalrpic)
+rookrpic = ("rookr.gif")
+rookrimg = Image(rookrpic)
+silver_generalrpic = ("silver_generalr.gif")
+silver_generalrimg = Image(silver_generalrpic)
+emptypic = ("empty.gif")
+emptyimg = Image(emptypic)
 
 
 
-
-#ROW2
-b21 = Button(f, "")
-b22 = Button(f, "")
-b23 = Button(f, "")
-b24 = Button(f, "")
-b25 = Button(f, "")
-b26 = Button(f, "")
-b27 = Button(f, "")
-b28 = Button(f, "")
-b29 = Button(f, "")
-
-
-
-#ROW3
-b31 = Button(f, "")
-b32 = Button(f, "")
-b33 = Button(f, "")
-b34 = Button(f, "")
-b35 = Button(f, "")
-b36 = Button(f, "")
-b37 = Button(f, "")
-b38 = Button(f, "")
-b39 = Button(f, "")
-
-
-
-#ROW4
-b41 = Button(f, "")
-b42 = Button(f, "")
-b43 = Button(f, "")
-b44 = Button(f, "")
-b45 = Button(f, "")
-b46 = Button(f, "")
-b47 = Button(f, "")
-b48 = Button(f, "")
-b49 = Button(f, "")
-
-
-
-
-#ROW5
-b51 = Button(f, "")
-b52 = Button(f, "")
-b53 = Button(f, "")
-b54 = Button(f, "")
-b55 = Button(f, "")
-b56 = Button(f, "")
-b57 = Button(f, "")
-b58 = Button(f, "")
-b59 = Button(f, "")
-
-
-#ROW6
-b61 = Button(f, "")
-b62 = Button(f, "")
-b63 = Button(f, "")
-b64 = Button(f, "")
-b65 = Button(f, "")
-b66 = Button(f, "")
-b67 = Button(f, "")
-b68 = Button(f, "")
-b69 = Button(f, "")
-
-
-
-
-#ROW7
-b71 = Button(f, "")
-b72 = Button(f, "")
-b73 = Button(f, "")
-b74 = Button(f, "")
-b75 = Button(f, "")
-b76 = Button(f, "")
-b77 = Button(f, "")
-b78 = Button(f, "")
-b79 = Button(f, "")
-
-
-
-#ROW8
-b81 = Button(f, "")
-b82 = Button(f, "")
-b83 = Button(f, "")
-b84 = Button(f, "")
-b85 = Button(f, "")
-b86 = Button(f, "")
-b87 = Button(f, "")
-b88 = Button(f, "")
-b89 = Button(f, "")
-
-
-
-
-
-
-#ROW9
-b91 = Button(f, "")
-b92 = Button(f, "")
-b93 = Button(f, "")
-b94 = Button(f, "")
-b95 = Button(f, "")
-b96 = Button(f, "")
-b97 = Button(f, "")
-b98 = Button(f, "")
-b99 = Button(f, "")
-
-
-#PLACING ON GRID
-
-#ROW1
-grid(b11, 1, 1)
-grid(b12, 1, 2)
-grid(b13, 1, 3)
-grid(b14, 1, 4)
-grid(b15, 1, 5)
-grid(b16, 1, 6)
-grid(b17, 1, 7)
-grid(b18, 1, 8)
-grid(b19, 1, 9)
-
-
-
-
-#ROW2
-grid(b21, 2, 1)
-grid(b22, 2, 2)
-grid(b23, 2, 3)
-grid(b24, 2, 4)
-grid(b25, 2, 5)
-grid(b26, 2, 6)
-grid(b27, 2, 7)
-grid(b28, 2, 8)
-grid(b29, 2, 9)
-
-
-
-#ROW3
-grid(b31, 3, 1)
-grid(b32, 3, 2)
-grid(b33, 3, 3)
-grid(b34, 3, 4)
-grid(b35, 3, 5)
-grid(b36, 3, 6)
-grid(b37, 3, 7)
-grid(b38, 3, 8)
-grid(b39, 3, 9)
-
-
-
-#ROW4
-grid(b41, 4, 1)
-grid(b42, 4, 2)
-grid(b43, 4, 3)
-grid(b44, 4, 4)
-grid(b45, 4, 5)
-grid(b46, 4, 6)
-grid(b47, 4, 7)
-grid(b48, 4, 8)
-grid(b49, 4, 9)
-
-
-#ROW5
-grid(b51, 5, 1)
-grid(b52, 5, 2)
-grid(b53, 5, 3)
-grid(b54, 5, 4)
-grid(b55, 5, 5)
-grid(b56, 5, 6)
-grid(b57, 5, 7)
-grid(b58, 5, 8)
-grid(b59, 5, 9)
-
-
-
-#ROW6
-grid(b61, 6, 1)
-grid(b62, 6, 2)
-grid(b63, 6, 3)
-grid(b64, 6, 4)
-grid(b65, 6, 5)
-grid(b66, 6, 6)
-grid(b67, 6, 7)
-grid(b68, 6, 8)
-grid(b69, 6, 9)
-
-
-#ROW7
-grid(b71, 7, 1)
-grid(b72, 7, 2)
-grid(b73, 7, 3)
-grid(b74, 7, 4)
-grid(b75, 7, 5)
-grid(b76, 7, 6)
-grid(b77, 7, 7)
-grid(b78, 7, 8)
-grid(b79, 7, 9)
-
-
-
-
-#ROW8
-grid(b81, 8, 1)
-grid(b82, 8, 2)
-grid(b83, 8, 3)
-grid(b84, 8, 4)
-grid(b85, 8, 5)
-grid(b86, 8, 6)
-grid(b87, 8, 7)
-grid(b88, 8, 8)
-grid(b89, 8, 9)
-
-
-
-#ROW9
-grid(b91, 9, 1)
-grid(b92, 9, 2)
-grid(b93, 9, 3)
-grid(b94, 9, 4)
-grid(b95, 9, 5)
-grid(b96, 9, 6)
-grid(b97, 9, 7)
-grid(b98, 9, 8)
-grid(b99, 9, 9)
-
-
-
-
-
-
-#=
-
-#FOR MINISHOGI
 
 w = Toplevel("Grid")
 f = Frame(w, padding = 10); pack(f, expand=true, fill="both")
 
 
-#ROW1
-b11 = Button(f, "")
-b12 = Button(f, "")
-b13 = Button(f, "")
-b14 = Button(f, "")
-b15 = Button(f, "")
-b16 = Button(f, "")
-b17 = Button(f, "")
-b18 = Button(f, "")
-b19 = Button(f, "")
+imgDict = Dict(('_','_')=>emptyimg,('k','b')=>kingimg,('b','b')=>bishopimg,('g','b')=>gold_generalimg,
+ ('n','b')=>knightimg,('l','b')=>lanceimg, ('B','b')=>promoted_bishopimg,('N','b')=>promoted_lanceimg,('P','b')=>promoted_pawnimg,
+ ('R','b')=>promoted_rookimg,('S','b')=>promoted_silver_generalimg,('r','b')=>rookimg,('s','b')=>silver_generalimg,
+ ('p','b')=>pawnimg,('k','w')=>kingrimg,('b','w')=>bishoprimg,('g','w')=>gold_generalrimg,
+  ('n','w')=>knightrimg,('l','w')=>lancerimg, ('B','w')=>promoted_bishoprimg,('N','w')=>promoted_lancerimg,('P','w')=>promoted_pawnrimg,
+  ('R','w')=>promoted_rookrimg,('S','w')=>promoted_silver_generalrimg,('r','w')=>rookrimg,('s','w')=>silver_generalrimg,
+  ('p','w')=>pawnrimg)
 
 
+pimg = Button(f, "",pawnimg)
 
 
-#ROW2
-b21 = Button(f, "")
-b22 = Button(f, "")
-b23 = Button(f, "")
-b24 = Button(f, "")
-b25 = Button(f, "")
-b26 = Button(f, "")
-b27 = Button(f, "")
-b28 = Button(f, "")
-b29 = Button(f, "")
-
-
-
-#ROW3
-b31 = Button(f, "")
-b32 = Button(f, "")
-b33 = Button(f, "")
-b34 = Button(f, "")
-b35 = Button(f, "")
-b36 = Button(f, "")
-b37 = Button(f, "")
-b38 = Button(f, "")
-b39 = Button(f, "")
-
-
-
-#ROW4
-b41 = Button(f, "")
-b42 = Button(f, "")
-b43 = Button(f, "")
-b44 = Button(f, "")
-b45 = Button(f, "")
-b46 = Button(f, "")
-b47 = Button(f, "")
-b48 = Button(f, "")
-b49 = Button(f, "")
-
-
-
-
-#ROW5
-b51 = Button(f, "")
-b52 = Button(f, "")
-b53 = Button(f, "")
-b54 = Button(f, "")
-b55 = Button(f, "")
-b56 = Button(f, "")
-b57 = Button(f, "")
-b58 = Button(f, "")
-b59 = Button(f, "")
-
-
-
-#PLACING ON GRID
-
-#ROW1
-grid(b11, 1, 1)
-grid(b12, 1, 2)
-grid(b13, 1, 3)
-grid(b14, 1, 4)
-grid(b15, 1, 5)
-grid(b16, 1, 6)
-grid(b17, 1, 7)
-grid(b18, 1, 8)
-grid(b19, 1, 9)
-
-
-
-
-#ROW2
-grid(b21, 2, 1)
-grid(b22, 2, 2)
-grid(b23, 2, 3)
-grid(b24, 2, 4)
-grid(b25, 2, 5)
-grid(b26, 2, 6)
-grid(b27, 2, 7)
-grid(b28, 2, 8)
-grid(b29, 2, 9)
-
-
-
-#ROW3
-grid(b31, 3, 1)
-grid(b32, 3, 2)
-grid(b33, 3, 3)
-grid(b34, 3, 4)
-grid(b35, 3, 5)
-grid(b36, 3, 6)
-grid(b37, 3, 7)
-grid(b38, 3, 8)
-grid(b39, 3, 9)
-
-
-
-#ROW4
-grid(b41, 4, 1)
-grid(b42, 4, 2)
-grid(b43, 4, 3)
-grid(b44, 4, 4)
-grid(b45, 4, 5)
-grid(b46, 4, 6)
-grid(b47, 4, 7)
-grid(b48, 4, 8)
-grid(b49, 4, 9)
-
-
-#ROW5
-grid(b51, 5, 1)
-grid(b52, 5, 2)
-grid(b53, 5, 3)
-grid(b54, 5, 4)
-grid(b55, 5, 5)
-grid(b56, 5, 6)
-grid(b57, 5, 7)
-grid(b58, 5, 8)
-grid(b59, 5, 9)
-
-
+#=bimg = Button(f, "",bishopimg)
+gimg = Button(f, "",gold_generalimg)
+nimg = Button(f, "",knightimg)
+limg = Button(f, "",lanceimg)
+bpimg = Button(f, "",promoted_bishopimg)
+npimg = Button(f, "",promoted_knightimg)
+lpimg = Button(f, "",promoted_lanceimg)
+ppimg = Button(f, "",promoted_pawnimg)
+rpimg = Button(f, "",promoted_rookimg)
+spimg = Button(f, "",promoted_silver_generalimg)
+rimg = Button(f, "",rookimg)
+simg = Button(f, "",silver_generalimg)
+emptypicimg = Button(f, "", emptyimg)
 
 =#
-
-
-
-
-#=
-
-#FOR CHU SHOGI
-
-
-
-w = Toplevel("Grid")
-f = Frame(w, padding = 10); pack(f, expand=true, fill="both")
-
-
-#ROW1
-b11 = Button(f, "")
-b12 = Button(f, "")
-b13 = Button(f, "")
-b14 = Button(f, "")
-b15 = Button(f, "")
-b16 = Button(f, "")
-b17 = Button(f, "")
-b18 = Button(f, "")
-b19 = Button(f, "")
-
-
-
-
-#ROW2
-b21 = Button(f, "")
-b22 = Button(f, "")
-b23 = Button(f, "")
-b24 = Button(f, "")
-b25 = Button(f, "")
-b26 = Button(f, "")
-b27 = Button(f, "")
-b28 = Button(f, "")
-b29 = Button(f, "")
-
-
-
-#ROW3
-b31 = Button(f, "")
-b32 = Button(f, "")
-b33 = Button(f, "")
-b34 = Button(f, "")
-b35 = Button(f, "")
-b36 = Button(f, "")
-b37 = Button(f, "")
-b38 = Button(f, "")
-b39 = Button(f, "")
-
-
-
-#ROW4
-b41 = Button(f, "")
-b42 = Button(f, "")
-b43 = Button(f, "")
-b44 = Button(f, "")
-b45 = Button(f, "")
-b46 = Button(f, "")
-b47 = Button(f, "")
-b48 = Button(f, "")
-b49 = Button(f, "")
-
-
-
-
-#ROW5
-b51 = Button(f, "")
-b52 = Button(f, "")
-b53 = Button(f, "")
-b54 = Button(f, "")
-b55 = Button(f, "")
-b56 = Button(f, "")
-b57 = Button(f, "")
-b58 = Button(f, "")
-b59 = Button(f, "")
-
-
-#ROW6
-b61 = Button(f, "")
-b62 = Button(f, "")
-b63 = Button(f, "")
-b64 = Button(f, "")
-b65 = Button(f, "")
-b66 = Button(f, "")
-b67 = Button(f, "")
-b68 = Button(f, "")
-b69 = Button(f, "")
-
-
-
-
-#ROW7
-b71 = Button(f, "")
-b72 = Button(f, "")
-b73 = Button(f, "")
-b74 = Button(f, "")
-b75 = Button(f, "")
-b76 = Button(f, "")
-b77 = Button(f, "")
-b78 = Button(f, "")
-b79 = Button(f, "")
-
-
-
-#ROW8
-b81 = Button(f, "")
-b82 = Button(f, "")
-b83 = Button(f, "")
-b84 = Button(f, "")
-b85 = Button(f, "")
-b86 = Button(f, "")
-b87 = Button(f, "")
-b88 = Button(f, "")
-b89 = Button(f, "")
-
-
-
-
-
-
-#ROW9
-b91 = Button(f, "")
-b92 = Button(f, "")
-b93 = Button(f, "")
-b94 = Button(f, "")
-b95 = Button(f, "")
-b96 = Button(f, "")
-b97 = Button(f, "")
-b98 = Button(f, "")
-b99 = Button(f, "")
-
-
-
-
-
-#ROW10
-b101 = Button(f, "")
-b102 = Button(f, "")
-b103 = Button(f, "")
-b104 = Button(f, "")
-b105 = Button(f, "")
-b106 = Button(f, "")
-b107 = Button(f, "")
-b108 = Button(f, "")
-b109 = Button(f, "")
-
-
-
-
-
-#ROW11
-b111 = Button(f, "")
-b112 = Button(f, "")
-b113 = Button(f, "")
-b114 = Button(f, "")
-b115 = Button(f, "")
-b116 = Button(f, "")
-b117 = Button(f, "")
-b118 = Button(f, "")
-b119 = Button(f, "")
-
-
-
-
-
-
-#ROW12
-b121 = Button(f, "")
-b122 = Button(f, "")
-b123 = Button(f, "")
-b124 = Button(f, "")
-b125 = Button(f, "")
-b126 = Button(f, "")
-b127 = Button(f, "")
-b128 = Button(f, "")
-b129 = Button(f, "")
-
-
-
-
-
-
-
-
-#PLACING ON GRID
-
-#ROW1
-grid(b11, 1, 1)
-grid(b12, 1, 2)
-grid(b13, 1, 3)
-grid(b14, 1, 4)
-grid(b15, 1, 5)
-grid(b16, 1, 6)
-grid(b17, 1, 7)
-grid(b18, 1, 8)
-grid(b19, 1, 9)
-
-
-
-
-#ROW2
-grid(b21, 2, 1)
-grid(b22, 2, 2)
-grid(b23, 2, 3)
-grid(b24, 2, 4)
-grid(b25, 2, 5)
-grid(b26, 2, 6)
-grid(b27, 2, 7)
-grid(b28, 2, 8)
-grid(b29, 2, 9)
-
-
-
-#ROW3
-grid(b31, 3, 1)
-grid(b32, 3, 2)
-grid(b33, 3, 3)
-grid(b34, 3, 4)
-grid(b35, 3, 5)
-grid(b36, 3, 6)
-grid(b37, 3, 7)
-grid(b38, 3, 8)
-grid(b39, 3, 9)
-
-
-
-#ROW4
-grid(b41, 4, 1)
-grid(b42, 4, 2)
-grid(b43, 4, 3)
-grid(b44, 4, 4)
-grid(b45, 4, 5)
-grid(b46, 4, 6)
-grid(b47, 4, 7)
-grid(b48, 4, 8)
-grid(b49, 4, 9)
-
-
-#ROW5
-grid(b51, 5, 1)
-grid(b52, 5, 2)
-grid(b53, 5, 3)
-grid(b54, 5, 4)
-grid(b55, 5, 5)
-grid(b56, 5, 6)
-grid(b57, 5, 7)
-grid(b58, 5, 8)
-grid(b59, 5, 9)
-
-
-
-#ROW6
-grid(b61, 6, 1)
-grid(b62, 6, 2)
-grid(b63, 6, 3)
-grid(b64, 6, 4)
-grid(b65, 6, 5)
-grid(b66, 6, 6)
-grid(b67, 6, 7)
-grid(b68, 6, 8)
-grid(b69, 6, 9)
-
-
-#ROW7
-grid(b71, 7, 1)
-grid(b72, 7, 2)
-grid(b73, 7, 3)
-grid(b74, 7, 4)
-grid(b75, 7, 5)
-grid(b76, 7, 6)
-grid(b77, 7, 7)
-grid(b78, 7, 8)
-grid(b79, 7, 9)
-
-
-
-
-#ROW8
-grid(b81, 8, 1)
-grid(b82, 8, 2)
-grid(b83, 8, 3)
-grid(b84, 8, 4)
-grid(b85, 8, 5)
-grid(b86, 8, 6)
-grid(b87, 8, 7)
-grid(b88, 8, 8)
-grid(b89, 8, 9)
-
-
-
-#ROW9
-grid(b91, 9, 1)
-grid(b92, 9, 2)
-grid(b93, 9, 3)
-grid(b94, 9, 4)
-grid(b95, 9, 5)
-grid(b96, 9, 6)
-grid(b97, 9, 7)
-grid(b98, 9, 8)
-grid(b99, 9, 9)
-
-
-
-
-#ROW10
-grid(b101, 10, 1)
-grid(b102, 10, 2)
-grid(b103, 10, 3)
-grid(b104, 10, 4)
-grid(b105, 10, 5)
-grid(b106, 10, 6)
-grid(b107, 10, 7)
-grid(b108, 10, 8)
-grid(b109, 10, 9)
-
-
-
-
-
-
-#ROW11
-grid(b111, 11, 1)
-grid(b112, 11, 2)
-grid(b113, 11, 3)
-grid(b114, 11, 4)
-grid(b115, 11, 5)
-grid(b116, 11, 6)
-grid(b117, 11, 7)
-grid(b118, 11, 8)
-grid(b119, 11, 9)
-
-
-
-
-
-#ROW12
-grid(b121, 12, 1)
-grid(b122, 12, 2)
-grid(b123, 12, 3)
-grid(b124, 12, 4)
-grid(b125, 12, 5)
-grid(b126, 12, 6)
-grid(b127, 12, 7)
-grid(b128, 12, 8)
-grid(b129, 12, 9)
-
-
-
-=#
+for x in 1:9
+  for y in 1:9
+    grid(Button(f, "",imgDict[board[x,y]]),x,y)
+  end
+end
