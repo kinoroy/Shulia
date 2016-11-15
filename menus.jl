7b669619617ac0fa51e54552009def4d36be3454
rezyder <rezyder>
2016-11-15 01:52:44 +0000

    game menu basics

---
+++ menus.jl
@@ -0,0 +1,60 @@
+w = Toplevel()
+tcl("pack", "propagate", w, false) ## or pack_stop_propagate(w)
+
+mb = Menu(w)            ## makes menu, adds to top-level window
+fmenu = menu_add(mb, "File")
+omenu = menu_add(mb, "Options")
+
+menu_add(fmenu, "Open file...", (path) -> println("Open file dialog, ..."))
+menu_add(fmenu, Separator(w))   ## second argument is Tk_Separator instance
+menu_add(fmenu, "Close window", (path) -> destroy(w))
+
+cb = Checkbutton(w, "Something visible")
+set_value(cb, false)     ## initialize
+menu_add(omenu, cb)     ## second argument is Tk_Checkbutton instance
+
+menu_add(omenu, Separator(w))   ## put in a separator
+
+rb = Radio(w, ["option 1", "option 2"])
+set_value(rb, "option 1")   ## initialize
+menu_add(omenu, rb)     ## second argument is Tk_Radio instance
+
+
+
+
+
+n = Button(w, "• Start a new game")
+o = Button(w, "• Continue an old game")
+r = Button(w, "• Replay a finished game")
+q = Button(w, "• Quit")
+
+
+
+
+pack(n, expand=true, fill="both")
+pack(o, expand=true, fill="both")
+pack(r, expand=true, fill="both")
+pack(q, expand=true, fill="both")
+
+
+
+
+
+function callback(path)
+  vals = map(get_value, (cb, rb))
+  println(vals)
+end
+
+
+function callbackr(path)
+  Messagebox(title="Quit", message="Are you sure you want to quit")
+end
+
+
+# do checkbutton for new game/continue
+
+
+callback_add(n, callback)   ## generic way to add callback for most common event
+callback_add(o, callback)
+callback_add(r, callback)
+callback_add(q, callbackr)
