ff622d8ab06efeefe434ab624d2424c99824dc99
rulai <rulai>
2016-12-07 02:15:26 +0000

    cs

--- game.jl
+++ game.jl
@@ -4,7 +4,8 @@ Authors: Kino, Amirrezera, Regan, Sam, Rulai
 #TO DO: Consoliate includes to top level (dparse,AIhelp , etc...)
 #............ more modules
 
-  include("menus.jl") #Start the GUI
+include("menus.jl") #Start the GUI
+
 while true
   sleep(1)
 end
